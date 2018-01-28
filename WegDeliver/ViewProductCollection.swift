fa1ef940d7891a0c3acd6e10ee0a7512839fa3fd
Tianyou Xiao <Robert@tianyous-macbook-pro.local>
2018-01-27 21:39:42 -0500

    AHA

--- WegDeliver/ViewProductCollection.swift
+++ WegDeliver/ViewProductCollection.swift
@@ -18,7 +18,7 @@ class ViewProductCollection: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        getProduct()
         //collectionView.dataSource = entreeDataSource
     }
     
@@ -48,53 +48,49 @@ class ViewProductCollection: UIViewController {
         var keyRequest = URLRequest(url: URL(string: "https://login.microsoftonline.com/1318d57f-757b-45b3-b1b0-9b3c3842774f/oauth2/token")!)
         keyRequest.httpMethod = "POST"
         keyRequest.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        do{
-            let code = "client_id=24960d97-4fbe-433d-ab8a-efeb89aa524e&grant_type=client_credentials&resource=https://wegmans-es.azure-api.net&client_secret=A8N7VeeCdFD5N4OxeQT1gFaXNStrxieEplYl3SYdxTs=".data(using:String.Encoding.ascii, allowLossyConversion: false)
-            keyRequest.httpBody = code
-            let task = URLSession.shared.dataTask(with: keyRequest) { (data: Data?, response: URLResponse?, error: Error?) in
-                if error != nil{
+        let code = "client_id=24960d97-4fbe-433d-ab8a-efeb89aa524e&grant_type=client_credentials&resource=https://wegmans-es.azure-api.net&client_secret=A8N7VeeCdFD5N4OxeQT1gFaXNStrxieEplYl3SYdxTs=".data(using:String.Encoding.ascii, allowLossyConversion: false)
+        keyRequest.httpBody = code
+        let task = URLSession.shared.dataTask(with: keyRequest) { (data: Data?, response: URLResponse?, error: Error?) in
+            if error != nil{
+                return
+            }
+            if let data = data{
+                var authorization = ""
+                do {
+                    let jsonObject: Any = try JSONSerialization.jsonObject(with: data, options: [])
+                    let jsonDict = jsonObject as! [String:String]
+                    let tokenType = jsonDict["token_type"]
+                    let accessToken = jsonDict["access_token"]
+                    authorization = tokenType! + " " + accessToken!
+                }
+                catch let error {
+                    print("JSON Parse Error")
                     return
                 }
-                if let data = data{
-                    var authorization = ""
-                    do {
-                        let jsonObject: Any = try JSONSerialization.jsonObject(with: data, options: [])
-                        let jsonDict = jsonObject as! [String:String]
-                        let tokenType = jsonDict["token_type"]
-                        let accessToken = jsonDict["access_token"]
-                        authorization = tokenType! + " " + accessToken!
-                    }
-                    catch let error {
-                        print("JSON Parse Error")
-                        return
-                    }
-                    
-                    //Request product info by search criterion
-                    let productSearchURL = URL(string: "https://wegmans-es.azure-api.net/productpublic/producthierarchies/Marketing/EZ Meals/Asian")!;
-                    
-                    var productRequest = URLRequest(url: productSearchURL)
-                    productRequest.httpMethod = "GET"
-                    productRequest.addValue("dbf9da0bc0814d07b6687136b4b36dd7", forHTTPHeaderField: "Product-Subscription-Key")
-                    productRequest.addValue(authorization, forHTTPHeaderField: "Authorization")
-                    
-                    URLSession.shared.dataTask(with: productRequest) { (data, response, error) in
-                        
-                        if let data = data,
-                            let string = String(data: data, encoding: .utf8) {
-                            print("Product Request")
-                            print(string)
-                        }else{
-                            print("failed")
-                        }
-                    }.resume()
-
-                }
                 
+                //Request product info by search criterion
+                let productSearchURL = URL(string: "https://wegmans-es.azure-api.net/productpublic/producthierarchies/Marketing/EZ%20Meals/Asian")!;
+                
+                var productRequest = URLRequest(url: productSearchURL)
+                productRequest.httpMethod = "GET"
+                productRequest.addValue("dbf9da0bc0814d07b6687136b4b36dd7", forHTTPHeaderField: "Product-Subscription-Key")
+                productRequest.addValue(authorization, forHTTPHeaderField: "Authorization")
+                
+                URLSession.shared.dataTask(with: productRequest) { (data, response, error) in
+                    
+                    if let data = data,
+                        let string = String(data: data, encoding: .utf8) {
+                        print("Product Request")
+                        print(string)
+                    }else{
+                        print("failed")
+                    }
+                }.resume()
+
             }
-            task.resume()
-        }catch{
-            print("POST for Auth Error")
+            
         }
+        task.resume()
         
     }
     
